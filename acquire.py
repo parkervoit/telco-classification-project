013d6e9714f1131476d1b8d15b10fff493db8b3f
parkervoit <parker.r.voit@gmail.com>
2021-05-27 16:07:42 -0500

    debugged and tested modules

--- acquire.py
+++ acquire.py
@@ -2,38 +2,25 @@ import pandas as pd
 import env
 import os
 # must have env.py saved in same directory as script. ensure the env.py is in your .gitignore
-def get_connection(db, user=env.username, host=env.host, password=env.password):
+def get_connection(db_name, username = env.username, host=env.host, password=env.password):
     '''
     This function makes a connection with and pulls from the CodeUp database. It 
     takes the database name as its argument, pulls other login info from env.py.
     Make sure you save this as a variable or it will print out your sensitive user
     info as plain text. 
     '''
-    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
-# these functions below will pull dbs as dataframes from the codeup database if it hasnt already
-# if it has, it will write a cache. 
-def get_titanic_data():
-    '''
-    This function pulls the titanic db into a dataframe if it doesnt exist, or it will cache 
-    the titanic data into a .csv file. 
-    '''
-    filename = 'titanic.csv'
-    if os.path.isfile(filename):
-        titanic_df = pd.read_csv(filename, index_col=0)
-        return titanic_df
-    else:
-        titanic_df = pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
-        titanic_df.to_csv(filename, mode = 'w+')
-        return titanic_df
+    return f'mysql+pymysql://{username}:{password}@{host}/{db_name}'
     
-def get_iris_data():
-    ''' This function pulls the iris db into a dataframe, or caches it as a .csv if it hasnt been already.
-    '''
-    filename = 'iris.csv'
+def get_telco_db(db_name, username = env.username, password = env.password, host = env.host):
+    filename = 'telco.csv'
     if os.path.isfile(filename):
-        iris_df = pd.read_csv(filename, index_col=0)
-        return iris_df
+        telco_df = pd.read_csv(filename, index_col=0)
+        return telco_df
     else:
-        iris_df = pd.read_sql('SELECT * FROM measurements JOIN species USING(species_id);', get_connection('iris_db'))
-        iris_df.to_csv(filename)
-        return iris_df
+        telco_df = pd.read_sql('''SELECT * FROM customers 
+                          JOIN internet_service_types USING(internet_service_type_id)
+                          JOIN contract_types USING(contract_type_id)
+                          JOIN payment_types USING (payment_type_id);''',
+                        get_connection('telco_churn'))
+        telco_df.to_csv(filename)
+        return telco_df
--- prepare.py
+++ prepare.py
@@ -1,41 +1,51 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.impute import SimpleImputer
+from sklearn.metrics import classification_report, confusion_matrix
 
-def prep_iris(iris_df, dummies):
+def train_validate_test_split(df, target, seed=123):
     '''
-    Takes in the iris_df, drops species id, and measurement id, then adds a dummy
-    variable column. It then returns the iris_df cleaned as iris_df
+    This function takes in a dataframe, the name of the target variable
+    (for stratification purposes), and an integer for a setting a seed
+    and splits the data into train, validate and test. 
+    Test is 20% of the original dataset, validate is .30*.80= 24% of the 
+    original dataset, and train is .70*.80= 56% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes. 
     '''
-    iris_df = iris_df.drop(columns = ['species_id', 'measurement_id'])
-    iris_df = iris_df.rename(columns = {'species_name':'species'})
-    if dummies == True:
-        iris_df = pd.get_dummies(data = iris_df, columns = ['species'], drop_first=True)
-    return iris_df
-# # Remember to delte your titanic .csv if you've already run this, or else it will break 
-def impute_mode(train, validate, test):
-    '''
-    impute mode for embark_town
-    '''
-    imputer = SimpleImputer(strategy='most_frequent', missing_values = None)
-    train[['embark_town']] = imputer.fit_transform(train[['embark_town']])
-    validate[['embark_town']] = imputer.transform(validate[['embark_town']])
-    test[['embark_town']] = imputer.transform(test[['embark_town']])
+    train_validate, test = train_test_split(df, test_size=0.2, 
+                                            random_state=seed, 
+                                            stratify=df[target])
+    train, validate = train_test_split(train_validate, test_size=0.3, 
+                                       random_state=seed,
+                                       stratify=train_validate[target])
     return train, validate, test
 
-def prep_titanic_data(df):
-    '''
-    takes in a dataframe of the titanic dataset as it is acquired and returns a cleaned dataframe
-    arguments: df: a pandas DataFrame with the expected feature names and columns
-    return: train, test, split: three dataframes with the cleaning operations performed on them
-    '''
-    df = df.drop_duplicates()
-    df = df.drop(columns=['deck', 'embarked', 'class', 'age', 'passenger_id'])
-    train, test = train_test_split(df, test_size=0.2, random_state=1349, stratify=df.survived)
-    train, validate = train_test_split(train, train_size=0.7, random_state=1349, stratify=train.survived)
-    train, validate, test = impute_mode(train, validate, test)
-    train = pd.get_dummies(data = train, columns = ['sex', 'embark_town'], drop_first=[True,True])
-    validate = pd.get_dummies(data = validate, columns = ['sex', 'embark_town'], drop_first=[True,True])
-    test = pd.get_dummies(data = test, columns = ['sex', 'embark_town'], drop_first=[True,True])
-    return train, validate, test
+def run_metrics(X, y, model, data_set = 'This'):
+    """
+    Takes in X , target as y, the model for testing, and the data_set(i.e. train, validate, test)\n
+    Outputs a print list with the confusion matrix, classification report, confusion matrix, and the T/F +/- rate
+    """
+    score = model.score(X, y)
+    matrix = confusion_matrix(y, model.predict(X))
+    tpr = matrix[1,1] / (matrix[1,1] + matrix[1,0])
+    fpr = matrix[0,1] / (matrix[0,1] + matrix[0,0])
+    tnr = matrix[0,0] / (matrix[0,0] + matrix[0,1])
+    fnr = matrix[1,0] / (matrix[1,1] + matrix[1,0])
+    prc = matrix[1,1] / (matrix[1,1] + matrix[0,1])
+    
+    print(f'{data_set} data set accuracy score: {score:.2%}')
+    print(f'{data_set} data set precision score {prc:.2%}')
+    print(f'{data_set} data set recall score: {tpr:.2%}\n')
+    class_report = classification_report(y, model.predict(X), zero_division=True)
+    print('-------------------------------')
+    print(f'classification report')
+    print(class_report)
+    print ('-------------------------------\n')
+    print('confusion matrix')
+    print(f'{matrix}\n')
+    print(f'{data_set} data set model metrics')
+    print('---------------------------------')
+    print(f'True positive rate for the model is {tpr:.2%}')
+    print(f'False positive rate for the model is  {fpr:.2%}')
+    print(f'True negative rate for the model is {tnr:.2%}')
+    print(f'False negative rate for the model is {fnr:.2%}\n')
